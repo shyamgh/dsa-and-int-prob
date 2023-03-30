9ca819ba154b8d7f27b5329ebef4fbaeb4e9d9fe
shyam-uptycs <shyamh@uptycs.com>
2023-03-31 05:17:11 +0530

    modified test1

--- int-problems/test1.py
+++ int-problems/test1.py
@@ -77,3 +77,61 @@ class A:
         print('static method')
     
 A.sm()
+
+
+# [1, 3, 5, 2, 2]
+# prfx =  1  4 9 11 13
+# pofx = 13 12 9  4  2
+    
+def find_index2(arr):
+
+    if len(arr) <= 2:
+        return -1
+
+    prs, pos = [], []
+    pos = [0 for i in range(len(arr))]
+    prs.append(arr[0])    
+    pos[-1] = arr[-1]
+
+    N = len(arr) - 1
+    i = 1
+    j = len(arr) - 2
+    
+    while i <= N:
+        prs.append(prs[i-1] + arr[i])
+        pos[j] = pos[j+1] + arr[j]     
+        i += 1
+        j -= 1
+
+    # while i >= 0:
+    #     pos.append(arr[i])
+    #     i -= 1
+    
+    print(prs)
+    print(pos)
+    
+    
+    for i in range(len(prs)):
+        if prs[i] == pos[i]:
+            return i
+    
+
+    return -1
+
+
+print(find_index2([1, 3, 5, 2, 2, 6]))
+
+
+
+
+
+
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('htps://www.makemytrip.com')
